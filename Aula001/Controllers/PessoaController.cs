4133891f3356159a6f8a15277472bbbccce5c456
NicholasCarv <nicholascarvalhobr@gmail.com>
2024-08-14 14:50:36 -0300

    Commit inicial com arquivos do projeto

---
+++ Aula001/Controllers/PessoaController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aula001.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PessoaController : ControllerBase
+    {
+ 
+        [HttpPost]
+        [Route("calcular-imc")]
+        public IActionResult CalcularIMC([FromBody] Pessoa pessoa)
+        {
+            double imc = pessoa.Peso / (pessoa.Altura * pessoa.Altura);
+            return Ok(new { IMC = imc });
+        }
+
+        [HttpGet]
+        [Route("consulta-tabela-imc")]
+        public IActionResult ConsultaTabelaIMC(double imc)
+        {
+            string classificacao = GetIMCClassificacao(imc);
+            return Ok(new { IMC = imc, Classificacao = classificacao });
+        }
+
+        private string GetIMCClassificacao(double imc)
+        {
+            if (imc < 18.5)
+                return "Abaixo do peso";
+            else if (imc >= 18.5 && imc < 24.9)
+                return "Peso normal";
+            else if (imc >= 25 && imc < 29.9)
+                return "Sobrepeso";
+            else if (imc >= 30 && imc < 34.9)
+                return "Obesidade Grau 1";
+            else if (imc >= 35 && imc < 39.9)
+                return "Obesidade Grau 2";
+            else
+                return "Obesidade Grau 3";
+        }
+    }
+
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+    }
+}
+
---
+++ Aula001/Controllers/freteController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourNamespace.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FreteController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CalcularFrete([FromBody] ProdutoInputModel produto)
+        {
+            if (produto == null)
+            {
+                return BadRequest("Produto não pode ser nulo.");
+            }
+
+            var tarifasPorEstado = new Dictionary<string, decimal>
+            {
+                { "SP", 50.00m },
+                { "RJ", 60.00m },
+                { "MG", 55.00m },
+                { "OUTROS", 70.00m }
+            };
+
+            decimal volume = (decimal)(produto.Altura * produto.Largura * produto.Comprimento);
+
+            var taxaPorCm3 = 0.01m;
+
+            if (!tarifasPorEstado.TryGetValue(produto.UF.ToUpper(), out decimal tarifaPorEstado))
+            {
+                tarifaPorEstado = tarifasPorEstado["OUTROS"];
+            }
+
+            var valorFrete = (volume * taxaPorCm3) + tarifaPorEstado;
+
+            return Ok(new { ValorFrete = valorFrete });
+        }
+    }
+
+
+    public class ProdutoInputModel
+    {
+        public string Nome { get; set; }
+        public decimal Peso { get; set; }
+        public decimal Altura { get; set; }
+        public decimal Largura { get; set; }
+        public decimal Comprimento { get; set; }
+        public string UF { get; set; }
+    }
+}
